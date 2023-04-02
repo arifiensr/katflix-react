4fa01dbeb2175bd6f94160aa5d6af45dc3f8c208
Muhammad Arifien Syachrizal <arifiensr@gmail.com>
2023-04-02 08:24:27 +0700

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from '@vitejs/plugin-react'
 //   plugins: [react()],
 // })
 export default defineConfig({
+  base: '/katflix-react/',
   plugins: [
     react({
       include: "**/*.jsx"
